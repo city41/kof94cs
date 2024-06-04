cd1db3e4e8fd92eb9e6f852b8451f093e79c4631
Matt Greer <matt.e.greer@gmail.com>
2024-06-04 13:14:10 -0400

    0.0.0

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # King of Fighters 94: Cheat Sheet
 
+![kof94 cheat sheet screenshot](https://github.com/city41/kof94cs/blob/main/screenshot.png?raw=true)
+
 A ROM hack that shows the special moves for the current player when you pause the game
+
+Requirements:
+
+- Playing in AES mode (as MVS mode does not have pause)
+- a single player game (versus mode won't show the moves)
--- scripts/makeIpsPatches.sh
+++ scripts/makeIpsPatches.sh
@@ -13,20 +13,28 @@ mkdir ipsBinaries/hacked
 yarn restore;
 cp $MAME_ROM_DIR/$GAME.zip ipsBinaries/original/
 
-yarn ts-node src/patchRom/main.ts
+yarn ts-node src/patchRom/main.ts src/patches/moveSetDuringPause.json
 cp $MAME_ROM_DIR/$GAME.zip ipsBinaries/hacked/
 
 (cd ipsBinaries/original/ && unzip $GAME.zip)
 (cd ipsBinaries/hacked/ && unzip $GAME.zip)
 
-# PROM='055-p1.p1'
-# SROM='055-s1.s1'
-# CROM1='055-c1.c1'
-# CROM2='055-c2.c2'
-
 for f in `ls ipsBinaries/original/ -I kof94.zip`; do
     bf=`basename $f`
-    yarn ts-node src/tools/makeIpsPatch.ts ipsBinaries/original/$bf ipsBinaries/hacked/$bf ipsPatches/$GAME.$bf.ips
+
+    originalFile="ipsBinaries/original/$bf"
+    hackedFile="ipsBinaries/hacked/$bf"
+
+    originalSha=$(sha256sum "$originalFile" | awk '{ print $1 }' )
+    hackedSha=$(sha256sum "$hackedFile" | awk '{ print $1 }' )
+
+    echo "$bf originalSha: $originalSha"
+    echo "$bf hackedSha: $hackedSha"
+
+    if [ "$originalSha" != "$hackedSha" ]; then
+        echo "Creating ips for $bf"
+        yarn ts-node src/tools/makeIpsPatch.ts ipsBinaries/original/$bf ipsBinaries/hacked/$bf ipsPatches/$GAME.$bf.ips
+    fi
 done
 
-(cd ipsPatches && zip kof94teIpsPatches.zip *.ips)
+(cd ipsPatches && zip kof94csIpsPatches.zip *.ips)
