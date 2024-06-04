dcfa0786090f2d2fb16babf2ce4d7c27cdff50f3
Matt Greer <matt.e.greer@gmail.com>
2024-06-04 12:20:03 -0400

    load the player current char moveset

---
+++ src/patches/charIdToMoveSet.asm
@@ -0,0 +1,28 @@
+; a simple table that maps from character ID to their moveset data
+
+dc.l $2HEIDERN
+dc.l $2RALF
+dc.l $2CLARK
+dc.l $2ATHENA
+dc.l $2SIE
+dc.l $2CHIN
+dc.l $2KYO
+dc.l $2BENI
+dc.l $2GORO
+dc.l $2HEAVY
+dc.l $2LUCKY
+dc.l $2BRIAN
+dc.l $2KIM
+dc.l $2CHANG
+dc.l $2CHOI
+dc.l $2TERRY
+dc.l $2ANDY
+dc.l $2JOE
+dc.l $2RYO
+dc.l $2ROBERT
+dc.l $2TAKUMA
+dc.l $2YURI
+dc.l $2MAI
+dc.l $2KING
+dc.l $2RUGAL
+dc.l $2SECFORM_R_U_G_A_L
--- src/patches/writeMoveset.asm
+++ src/patches/writeMoveset.asm
@@ -1,9 +1,38 @@
 ;; writeMoveset
 ;;
 ;; write's the current player's character's moveset to the fix layer
+;; but only in single player mode
 
 movem.l A0/D0, $MOVEM_STORAGE
-lea $2TERRY, A0
+
+lea $2CHARID_TO_MOVE_SET, A0
+
+;; is P1 playing?
+cmpi.b #1, $BIOS_PLAYER_MOD1
+bne loadPlayer2
+;; ok player 1 is playing. but is this versus mode?
+cmpi.b #1, $BIOS_PLAYER_MOD2
+beq done ; it is versus mode, nothing to do
+
+;; single player, p1 game
+clr.w D0
+move.b $108171, D0 ; load player 1's current character's ID
+add.w D0, D0  ; quadruple it as the table is of longs
+add.w D0, D0  ; quadruple it as the table is of longs
+adda.w D0, A0 ; move forward in the table to the character's pointer
+
+bra doneLoadPlayerPointer
+
+loadPlayer2:
+;; we already know p1 is not playing
+;; single player, p2 game
+move.b $108371, D0 ; load player 2's current character's ID
+add.w D0, D0  ; quadruple it as the table is of longs
+add.w D0, D0  ; quadruple it as the table is of longs
+adda.w D0, A0 ; move forward in the table to the character's pointer
+
+doneLoadPlayerPointer:
+movea.l (A0), A0 ; go from the pointer to the actual data
 
 writeChar:
 move.l (A0)+, D0
