e2f60e87aa014a611296f7ef6c50057757ca4c3b
Matt Greer <matt.e.greer@gmail.com>
2024-06-26 07:15:20 -0400

    makeIpsPatches.sh improvements

--- scripts/makeIpsPatches.sh
+++ scripts/makeIpsPatches.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 GAME='kof94'
+HACK='cs'
 
 rm -rf ipsBinaries
 rm -rf ipsPatches
@@ -11,30 +12,30 @@ mkdir ipsBinaries/original
 mkdir ipsBinaries/hacked
 
 yarn restore;
-cp $MAME_ROM_DIR/$GAME.zip ipsBinaries/original/
+cp ${MAME_ROM_DIR}/${GAME}.zip ipsBinaries/original/
 
 yarn ts-node src/patchRom/main.ts src/patches/kof94cs.json
-cp $MAME_ROM_DIR/$GAME.zip ipsBinaries/hacked/
+cp ${MAME_ROM_DIR}/${GAME}.zip ipsBinaries/hacked/
 
-(cd ipsBinaries/original/ && unzip $GAME.zip)
-(cd ipsBinaries/hacked/ && unzip $GAME.zip)
+(cd ipsBinaries/original/ && unzip ${GAME}.zip)
+(cd ipsBinaries/hacked/ && unzip ${GAME}.zip)
 
-for f in `ls ipsBinaries/original/ -I kof94.zip`; do
-    bf=`basename $f`
+for f in `ls ipsBinaries/original/ -I ${GAME}.zip`; do
+    bf=`basename ${f}`
 
-    originalFile="ipsBinaries/original/$bf"
-    hackedFile="ipsBinaries/hacked/$bf"
+    originalFile="ipsBinaries/original/${bf}"
+    hackedFile="ipsBinaries/hacked/${bf}"
 
-    originalSha=$(sha256sum "$originalFile" | awk '{ print $1 }' )
-    hackedSha=$(sha256sum "$hackedFile" | awk '{ print $1 }' )
+    originalSha=$(sha256sum "${originalFile}" | awk '{ print $1 }' )
+    hackedSha=$(sha256sum "${hackedFile}" | awk '{ print $1 }' )
 
-    echo "$bf originalSha: $originalSha"
-    echo "$bf hackedSha: $hackedSha"
+    echo "${bf} originalSha: ${originalSha}"
+    echo "${bf} hackedSha: ${hackedSha}"
 
-    if [ "$originalSha" != "$hackedSha" ]; then
-        echo "Creating ips for $bf"
-        yarn ts-node src/tools/makeIpsPatch.ts ipsBinaries/original/$bf ipsBinaries/hacked/$bf ipsPatches/$GAME.$bf.ips
+    if [ "${originalSha}" != "${hackedSha}" ]; then
+        echo "Creating ips for ${bf}"
+        yarn ts-node src/tools/makeIpsPatch.ts ipsBinaries/original/${bf} ipsBinaries/hacked/${bf} ipsPatches/${GAME}${HACK}.${bf}.ips
     fi
 done
 
-(cd ipsPatches && zip kof94csIpsPatches.zip *.ips)
+(cd ipsPatches && zip ${GAME}${HACK}IpsPatches.zip *.ips)
